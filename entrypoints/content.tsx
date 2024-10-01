490350d28ce07630d37f1e24015ac0943f9d23e3
Tea713 <thanhnguyen_2025@depauw.edu>
2024-10-01 02:13:38 -0400

    Display Readability.js content of pages

--- entrypoints/content.tsx
+++ entrypoints/content.tsx
@@ -1,5 +1,7 @@
 import Tooltip from "@/components/Tooltip";
 import ReactDOM from "react-dom/client";
+import { Readability } from "@mozilla/readability";
+import DOMPurify from "dompurify";
 import "@/assets/main.css";
 
 export default defineContentScript({
@@ -20,7 +22,9 @@ export default defineContentScript({
       document.body.appendChild(tooltipDiv);
 
       root = ReactDOM.createRoot(tooltipDiv);
-      root.render(<Tooltip text={extractedContent ?? ""} position={position} />);
+      root.render(
+        <Tooltip text={extractedContent ?? ""} position={position} />
+      );
     }
 
     function hideTooltip() {
@@ -32,17 +36,39 @@ export default defineContentScript({
       }
     }
 
+    async function getReadabilityContent(url: string) {
+      const response: Response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch data. Response status: ${response.status}`
+        );
+      }
+      const html = await response.text();
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const reader = new Readability(doc);
+      const article = reader.parse();
+      if (article) {
+        return DOMPurify.sanitize(article.content);
+      } else {
+        console.error("Fail to parse content.");
+        return null;
+      }
+    }
+
     document.querySelectorAll("a").forEach((element: HTMLAnchorElement) => {
-      element.addEventListener("mouseover", () => {
+      element.addEventListener("mouseover", async () => {
         element.style.backgroundColor = "yellow";
         const rect: DOMRect = element.getBoundingClientRect();
-        const position = {x: rect.left, y: rect.bottom};
-
-        chrome.runtime.sendMessage({action: 'fetch-content', url: element.href}, (response) => {
-          if(response?.extractedContent) {
-            showTooltip(response.extractedContent, position);
-          }
-        })
+        const position = { x: rect.left, y: rect.bottom };
+        const url = element.href;
+        const readabilityContent = await getReadabilityContent(url);
+        if(readabilityContent) {
+          showTooltip(readabilityContent, position);
+        } else {
+          console.error(`Fail to fetch content from ${url}.`)
+        }
       });
 
       element.addEventListener("mouseout", () => {
